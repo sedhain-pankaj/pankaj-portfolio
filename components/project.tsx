ac2fe619dd7fdddab7cf8e748062eecd97ede2e9
Pankaj Sedhain <kalpanakarki.ps@gmail.com>
2024-02-24 21:34:14 +1100

    onClick for photos

--- components/project.tsx
+++ components/project.tsx
@@ -54,7 +54,8 @@ export default function Project({
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+          onClick={() => window.open(imageUrl.src, "_blank")}
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl cursor-pointer
         transition 
         group-hover:scale-110
         group-hover:-translate-x-3
