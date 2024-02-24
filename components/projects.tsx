b2e1f3402719e5ebf764062723b5f9e128223631
Pankaj Sedhain <kalpanakarki.ps@gmail.com>
2024-02-24 20:03:43 +1100

    projects component

---
+++ components/projects.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "./project";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="scroll-mt-28 mb-28">
+      <SectionHeading>My Projects</SectionHeading>
+
+      <div>
+        {projectsData.map((project) => (
+            <React.Fragment key={project.title}>
+                <Project {...project} />
+            </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
