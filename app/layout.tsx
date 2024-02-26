1bafc10a77ee0e2f7595da633dd6b1e77cc9aa46
Pankaj Sedhain <kalpanakarki.ps@gmail.com>
2024-02-26 12:54:06 +1100

    added context-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import ActiveSectionContentProvider from "@/context/active-section-context";
+import Footer from "@/components/footer";
+import ThemeContextProvider from "@/context/theme-context";
+import ThemeSwitch from "@/components/theme-switch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,14 +20,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+        className={`${inter.className} bg-gray-50 text-gray-950 pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#fcd1d3] fixed top-[-6rem] -z-10 right-[10rem] h-[35rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
-        <div className="bg-[#dfdbfb] fixed top-[-1rem] -z-10 left-[-35rem] h-[35rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-        <Header />
-        {children}
+        <div className="relative">
+          <div className="bg-[#fcd1d3] fixed top-[-6rem] -z-10 right-[10rem] h-[35rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+          <div className="bg-[#dfdbfb] fixed top-[-1rem] -z-10 left-[-35rem] h-[35rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+        </div>
+
+        <ActiveSectionContentProvider>
+          <ThemeContextProvider>
+            <Header />
+            {children}
+            <Footer />
+            <ThemeSwitch />
+          </ThemeContextProvider>
+        </ActiveSectionContentProvider>
       </body>
     </html>
   );
