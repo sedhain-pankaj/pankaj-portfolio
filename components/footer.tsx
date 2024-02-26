b27929746dcad1d6fcb429654b3aa6f9ff56358b
Pankaj Sedhain <kalpanakarki.ps@gmail.com>
2024-02-26 13:13:19 +1100

    footer theme error removed using flex

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,15 +2,17 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mb-10 px-4 text-center text-gray-500">
-      <small className="mb-2 block text-xs">
-        &copy; 2030 Pankaj Sedhain. All rights reserved.
-      </small>
-      <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React & NextJS 14 (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, hosted on Vercel.
-      </p>
+    <footer className="flex justify-center">
+      <div className="mb-5 px-4 text-center text-gray-500">
+        <small className="mb-2 block text-xs">
+          &copy; 2030 Pankaj Sedhain. All rights reserved.
+        </small>
+        <p className="text-xs">
+          <span className="font-semibold">About this website:</span> built with
+          React & NextJS 14 (App Router & Server Actions), TypeScript, Tailwind
+          CSS, Framer Motion, React Email & Resend, hosted on Vercel.
+        </p>
+      </div>
     </footer>
   );
 }
