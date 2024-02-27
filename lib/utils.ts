db1e6bd413931931bb6d99414ab952a3f4582966
Pankaj Sedhain <kalpanakarki.ps@gmail.com>
2024-02-27 11:56:22 +1100

    email validation utility

---
+++ lib/utils.ts
@@ -0,0 +1,26 @@
+export const validateString = (
+  value: unknown,
+  maxLength: number
+): value is string => {
+  if (!value || typeof value !== "string" || value.length > maxLength) {
+    return false;
+  }
+
+  return true;
+};
+
+export const getErrorMessage = (error: unknown): string => {
+  if (error instanceof Error) {
+    return error.message;
+  }
+
+  if (error && typeof error === "object" && "message" in error) {
+    return String(error.message);
+  }
+
+  if (typeof error === "string") {
+    return error;
+  }
+
+  return "Something went wrong";
+};
