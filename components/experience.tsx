8452dad3748d24cb22a01267f8b1faed427937b2
Pankaj Sedhain <kalpanakarki.ps@gmail.com>
2024-02-28 09:45:07 +1100

    timeline ripped seperate for exp to reuse in edu

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,76 +1,15 @@
-"use client";
-
-import React, { useRef } from "react";
-import SectionHeading from "./section-heading";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
+import React from "react";
+import TimelineTree from "./timeline";
 import { experiencesData } from "@/lib/data";
-import { useSectionInView } from "@/lib/hooks";
-import { useTheme } from "@/context/theme-context";
-import { motion, useScroll, useTransform } from "framer-motion";
 
-export default function Experience() {
-  const { ref } = useSectionInView("Experience", 0.35);
-  const { theme } = useTheme();
-
-  const ref2 = useRef<HTMLDivElement>(null);
-  const { scrollYProgress } = useScroll({
-    target: ref2,
-    offset: ["0 1", "1.1 1"],
-  });
-  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
-
+const Experience: React.FC = () => {
   return (
-    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
-      <SectionHeading>My experience</SectionHeading>
-      <motion.div
-        ref={ref2}
-        style={{
-          scale: scaleProgess,
-          opacity: opacityProgess,
-        }}
-      >
-        <VerticalTimeline lineColor="" animate={false}>
-          {experiencesData.map((item, index) => (
-            <React.Fragment key={index}>
-              <VerticalTimelineElement
-                visible
-                contentStyle={{
-                  background:
-                    theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
-                  boxShadow: "none",
-                  border: "1px solid rgba(0, 0, 0, 0.05)",
-                  borderRadius: "1rem",
-                  textAlign: "left",
-                  padding: "1.3rem 2rem",
-                }}
-                contentArrowStyle={{
-                  borderRight:
-                    theme === "light"
-                      ? "0.4rem solid #9ca3af"
-                      : "0.4rem solid rgba(255, 255, 255, 0.5)",
-                }}
-                date={item.date}
-                icon={item.icon}
-                iconStyle={{
-                  background: theme === "light" ? "white" : "#a8a29e",
-                  fontSize: "1.5rem",
-                }}
-              >
-                <h3 className="font-semibold capitalize text-xl">{item.title}</h3>
-                <p className="font-normal !mt-1 underline">{item.location}</p>
-                <p className="!mt-2 !font-normal text-gray-700 dark:text-white/75">
-                  {item.description}
-                </p>
-              </VerticalTimelineElement>
-            </React.Fragment>
-          ))}
-        </VerticalTimeline>
-      </motion.div>
-    </section>
+    <TimelineTree
+      data={experiencesData}
+      sectionId="Experience"
+      sectionTitle="My experience"
+    />
   );
-}
+};
+
+export default Experience;
---
+++ components/timeline.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import React, { useRef } from "react";
+import SectionHeading from "./section-heading";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { useSectionInView } from "@/lib/hooks";
+import { useTheme } from "@/context/theme-context";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { EducationDetails, ExperienceDetails, SectionName } from "@/lib/types";
+
+interface TimelineTreeProps {
+  data: Array<EducationDetails | ExperienceDetails>;
+  sectionId: SectionName;
+  sectionTitle: string;
+}
+
+const TimelineTree: React.FC<TimelineTreeProps> = ({
+  data,
+  sectionId,
+  sectionTitle,
+}) => {
+  const { ref } = useSectionInView(sectionId);
+  const { theme } = useTheme();
+
+  const ref2 = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref2,
+    offset: ["0 1", "1.1 1"],
+  });
+  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+
+  return (
+    <section id={sectionId} ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
+      <SectionHeading>{sectionTitle}</SectionHeading>
+      <motion.div
+        ref={ref2}
+        style={{
+          scale: scaleProgess,
+          opacity: opacityProgess,
+        }}
+      >
+        <VerticalTimeline lineColor="" animate={false}>
+          {data.map((item, index) => (
+            <React.Fragment key={index}>
+              <VerticalTimelineElement
+                visible
+                contentStyle={{
+                  background:
+                    theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+                  boxShadow: "none",
+                  border: "1px solid rgba(0, 0, 0, 0.05)",
+                  borderRadius: "1rem",
+                  textAlign: "left",
+                  padding: "1.3rem 2rem",
+                }}
+                contentArrowStyle={{
+                  borderRight:
+                    theme === "light"
+                      ? "0.4rem solid #9ca3af"
+                      : "0.4rem solid rgba(255, 255, 255, 0.5)",
+                }}
+                date={item.date}
+                icon={item.icon}
+                iconStyle={{
+                  background: theme === "light" ? "white" : "#a8a29e",
+                  fontSize: "1.5rem",
+                }}
+              >
+                <h3 className="font-semibold capitalize text-xl">
+                  {item.title}
+                </h3>
+                <p className="font-normal !mt-1 underline">{item.location}</p>
+                <p className="!mt-2 !font-normal text-gray-700 dark:text-white/75">
+                  {item.description}
+                </p>
+              </VerticalTimelineElement>
+            </React.Fragment>
+          ))}
+        </VerticalTimeline>
+      </motion.div>
+    </section>
+  );
+};
+
+export default TimelineTree;
